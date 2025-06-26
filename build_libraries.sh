cd270126b700eb60b5690e91d9249161f7a86e7d
ianchb <i@4t.pw>
2025-06-26 09:36:20 +0800

    correct ncurses build options

--- build_libraries.sh
+++ build_libraries.sh
@@ -207,7 +207,7 @@ build_library "sqlite" "${SQLITE_URL}" "autoconf-3500000" \
 
 # 5. ncurses
 build_library "ncurses" "${NCURSES_URL}" "${NCURSES_VERSION}" \
-    "--with-shared --with-termlib --with-terminfo-dirs=\"/usr/share/terminfo:/lib/terminfo:/etc/terminfo\" --with-pkg-config-libdir=$HOME/cross-compile/install/ncurses/lib/pkgconfig --without-debug --enable-widec --enable-pc-files --enable-overwrite --with-strip-program=$HOME/x-tools/arm-ev3-linux-gnueabi/bin/arm-ev3-linux-gnueabi-strip"
+    "--with-shared --with-termlib --with-terminfo-dirs=\"/usr/share/terminfo:/lib/terminfo:/etc/terminfo\" --with-pkg-config-libdir=/usr/lib/pkgconfig --without-debug --enable-widec --enable-pc-files --enable-overwrite --with-strip-program=$HOME/x-tools/arm-ev3-linux-gnueabi/bin/arm-ev3-linux-gnueabi-strip"
 
 # 6. readline
 build_library "readline" "${READLINE_URL}" "${READLINE_VERSION}" \
